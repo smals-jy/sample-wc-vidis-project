eef3f4900737308e6fbb5319cdc3d323af32ffe1
Jacques Yakoub <jacques.yakoub@smals.be>
2025-03-15 13:16:42 +0100

    ci: vite config

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// https://vite.dev/guide/static-deploy.html#github-pages
+
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+   base: process.env.GITHUB_BASE || "/"
+})
