84729d15272c4478e474bfa78a61f7eaa1e1b76b
Jacques Yakoub <jacques.yakoub@gmail.com>
2025-03-15 21:04:55 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: process.env.GITHUB_BASE || "/"
+   base: process.env.GITHUB_URL || "/"
 })
