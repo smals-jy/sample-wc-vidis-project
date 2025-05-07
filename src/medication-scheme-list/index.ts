64b520a20da70655c4e4a307f6f5e13552dda352
smals-jy <143497095+smals-jy@users.noreply.github.com>
2025-05-07 15:38:13 +0200

    Update index.ts

--- src/medication-scheme-list/index.ts
+++ src/medication-scheme-list/index.ts
@@ -3,12 +3,12 @@ import "@smals-belgium-shared/vidis-medication-scheme-list"
 // Import CSS
 import "@smals-belgium-shared/vidis-medication-scheme-list/medication-scheme-list.css"
 // TODO will be removed when VIDIS packages have built-in types (needed so I can review MAGS criteria)
-import type { MedicationScheme, Parameters } from "../@types/app";
+import type { MedicationSchemeList, Parameters } from "../@types/app";
 
 // To int the component
 export default async function initModule(params : Parameters) {
     
-    const wc = document.createElement("vidis-medication-scheme-list") as MedicationScheme;
+    const wc = document.createElement("vidis-medication-scheme-list") as MedicationSchemeList;
 
     // Common inputs for all VIDIS web components
     // Refer to https://www.npmjs.com/package/@smals-belgium/myhealth-wc-integration for more details
