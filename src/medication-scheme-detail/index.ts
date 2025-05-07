2243dd68d9a63f8237ed8a57984b7c199b848602
smals-jy <143497095+smals-jy@users.noreply.github.com>
2025-05-07 15:25:14 +0200

    Create index.ts

---
+++ src/medication-scheme-detail/index.ts
@@ -0,0 +1,5 @@
+// Import JS
+import "@smals-belgium-shared/vidis-medication-scheme-detail"
+// Import CSS
+import "@smals-belgium-shared/vidis-medication-scheme-detail/medication-scheme-detail.css"
+// TODO
