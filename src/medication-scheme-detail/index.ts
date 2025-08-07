997b8117ab1d911d941076e923b7f903540c0f69
smals-jy <143497095+smals-jy@users.noreply.github.com>
2025-08-07 16:58:28 +0200

    Update index.ts

--- src/medication-scheme-detail/index.ts
+++ src/medication-scheme-detail/index.ts
@@ -18,7 +18,7 @@ export default async function initModule(params : Parameters) {
     wc.professional = false;
     wc.offlineDataStorageEnabled = false;
     wc.isOfflineAuthenticated = false;
-    wc.ssin = undefined;
+    wc.ssin = "12987654321";
 
     // Specific input for this common
     // Here is a dummy place holder id, to check what happens when medication list item doesn't exist anymore
