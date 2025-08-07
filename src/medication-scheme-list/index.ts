c5ffcc7d528b2b32957184af66f8d903ef1c0c84
smals-jy <143497095+smals-jy@users.noreply.github.com>
2025-08-07 16:58:46 +0200

    Update index.ts

--- src/medication-scheme-list/index.ts
+++ src/medication-scheme-list/index.ts
@@ -18,6 +18,7 @@ export default async function initModule(params : Parameters) {
     wc.professional = false;
     wc.offlineDataStorageEnabled = false;
     wc.isOfflineAuthenticated = false;
+    wc.ssin = "12987654321";
 
     return wc;
 }
